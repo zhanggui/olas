752f9212901a72e1162ccb509ddbb17215b53a2d
zhanggui <zhanggui@marchsoft.cn>
2014-10-31 13:22:42 +0800

    first bush

---
+++ olasiphone/trunk/tsansweriphone/tsansweriphone/ViewController/BaseViewController/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  tsansweriphone
+//
+//  Created by xiaoguizi on 14-9-8.
+//  Copyright (c) 2014年 xiaoguizi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
---
+++ olasiphone/trunk/tsansweriphone/tsansweriphone/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tsansweriphone
+//
+//  Created by xiaoguizi on 14-9-8.
+//  Copyright (c) 2014年 xiaoguizi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
